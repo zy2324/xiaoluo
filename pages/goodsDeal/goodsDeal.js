14cbfa39eba0bb0836e088d269a150e48ab0a02e
zy2324 <806897446@qq.com>
2019-06-23 21:44:40 +0800

    deal init

--- pages/goodsDeal/goodsDeal.js
+++ pages/goodsDeal/goodsDeal.js
@@ -4,25 +4,92 @@ Page({
   data: {
     name: '',
     price: '',
-    short:'',
-    gid: ''
+    short: '',
+    gid: '',
+    date: '',
+    time: '',
+    buyerphonenum: '',
+    dealsit: '',
+    show: false,
+    via: ['http://a1.qpic.cn/psb?/V108803N2OnUxr/vvGx*nGekiHIYe8Q*TOH177u1Rb4EsgMNCXfiXgTSX4!/b/dMAAAAAAAAAA&ek=1&kp=1&pt=0&bo=OATJBQAAAAARF9A!&tl=3&vu']
   },
 
-  bindDateChange: function (e) {
+  dealsiteChange: function(e) {
+    this.setData({
+      dealsite: e.detail.value
+    })
+  },
+
+  buyerChange: function(e) {
+    this.setData({
+      buyerphonenum: e.detail.value
+    })
+  },
+
+  bindDateChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       date: e.detail.value
     })
   },
 
-  bindTimeChange: function (e) {
+  bindTimeChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       time: e.detail.value
     })
   },
 
-  onLoad: function (options) {
+  handleImagePreview(e) {
+    const idx = e.target.dataset.idx
+    const images = this.data.via
+    wx.previewImage({
+      current: images[idx],  //当前预览的图片
+      urls: images,  //所有要预览的图片
+    })
+  },
+
+  showVia: function() {
+    this.setData({ show: true });
+  },
+
+  pushdealok: function() {
+
+  },
+
+  formSubmit: function(){
+
+  },
+
+  makeDeal: function() {
+    //支付接口，并确认支付是否成功
+
+
+
+    //支付成功，则将新增的数据传入后端
+    wx.request({
+      url: 'http://47.95.237.94:8001/api/v1/goods/dealok',
+      data: {
+        openId: app.globalData.openId,
+        gid: this.data.gid,
+        dealtime: this.data.date + " " + this.data.time,
+        buyerphonenum: this.data.buyerphonenum,
+        dealsite: this.data.dealsite,
+        state: "2",
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "POST",
+      success: function(res) {
+        console.log(res.data)
+      }
+    })
+
+    //成功后，推送消息
+  },
+
+  onLoad: function(options) {
     var self = this;
     wx.request({
       url: 'http://47.95.237.94:8001/api/v1/goods/' + options.gid + '/detail',
